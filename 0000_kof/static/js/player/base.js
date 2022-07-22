04f0fb152fa18a0a971ba8c11d57b2bf99fdb55c
lyn <liuyunuo1018@163.com>
2022-07-22 14:12:38 +0800

    add collision attack functino

--- 0000_kof/static/js/player/base.js
+++ 0000_kof/static/js/player/base.js
@@ -11,6 +11,10 @@ export class Player extends SupGameObject {
         this.height = info.height;
         this.color = info.color;
 
+        this.hp = 100;  // 血量
+        this.$hp = this.root.$kof.find(`.kof-head-hp-${this.id}>div`);
+        this.$hp_div = this.$hp.find('div');
+
         this.direction = 1;  // 方向
 
         this.vx = info.vx;
@@ -35,10 +39,7 @@ export class Player extends SupGameObject {
     }
 
     update_move() {
-        if (this.status === 3) {
-            this.vy += this.gravity;
-        }
-
+        this.vy += this.gravity;
 
         this.x += this.vx * this.timedelta / 1000;
         this.y += this.vy * this.timedelta / 1000;
@@ -102,6 +103,8 @@ export class Player extends SupGameObject {
     }
 
     update_direction() {
+        if (this.status === 6) return;  // 如果处于死亡状态就不会再变方向
+
         let players = this.root.players;
         if (players[0] && players[1]) {
             let me = this, you = players[1 - this.id];
@@ -110,17 +113,91 @@ export class Player extends SupGameObject {
         }
     }
 
+    is_attack() {  // 被攻击到
+        if (this.status === 6) return;  // 如果已经死亡就返回
+
+        this.status = 5;  // 变为被攻击转态
+        this.frame_current_cnt = 0;
+
+        this.hp = Math.max(this.hp - 20, 0);
+
+        this.$hp_div.animate({
+            width: this.$hp.parent().width() * this.hp / 100
+        }, 300);
+        this.$hp.animate({
+            width: this.$hp.parent().width() * this.hp / 100
+        }, 600);
+
+        if (this.hp <= 0) {
+            this.status = 6;
+            this.frame_current_cnt = 0;
+            this.vx = 0;
+        }
+    }
+
+    is_collision(r1, r2) {  // 碰撞检测
+        if (Math.max(r1.x1, r2.x1) > Math.min(r1.x2, r2.x2))
+            return false;
+        if (Math.max(r1.y1, r2.y1) > Math.min(r1.y2, r2.y2))
+            return false;
+
+        return true;
+    }
+
+    update_attack() {
+        if (this.status === 4 && this.frame_current_cnt === 18) {
+            let me = this, you = this.root.players[1 - this.id];
+            let r1;
+            if (this.direction > 0) {
+                r1 = {
+                    x1: me.x + 120,
+                    y1: me.y + 40,
+                    x2: me.x + 120 + 100,
+                    y2: me.y + 40 + 20,
+                };
+            } else {
+                r1 = {
+                    x1: me.x + me.width - 120 - 100,
+                    y1: me.y + 40,
+                    x2: me.x + me.width - 120 - 100 + 100,
+                    y2: me.y + 40 + 20,
+                }
+            }
+
+            let r2 = {
+                x1: you.x,
+                y1: you.y,
+                x2: you.x + you.width,
+                y2: you.y + you.height,
+            }
+
+            if (this.is_collision(r1, r1)) {  // 若果攻击到对手
+                you.is_attack();
+            }
+        }
+    }
+
     update() {
         this.update_control();
         this.update_move();
         this.update_direction();
+        this.update_attack();
 
         this.render();
     }
 
     render() {
-        // this.ctx.fillStyle = this.color;
+        // 碰撞调试
+        // this.ctx.fillStyle = 'blue';
         // this.ctx.fillRect(this.x, this.y, this.width, this.height);
+
+        // if (this.direction > 0) {
+        //     this.ctx.fillStyle = 'red';
+        //     this.ctx.fillRect(this.x + 120, this.y + 40, 100, 20);
+        // } else {
+        //     this.ctx.fillStyle = 'red';
+        //     this.ctx.fillRect(this.x + this.width - 120 - 100, this.y + 40, 100, 20);
+        // }
 
         let status = this.status;
         if (this.status === 1 && this.direction * this.vx < 0) status = 2;
@@ -146,9 +223,13 @@ export class Player extends SupGameObject {
 
         }
 
-        if (status === 4) {
+        if (status === 4 || status === 5 || status === 6) {
             if (this.frame_current_cnt === obj.frame_rate * (obj.frame_cnt - 1)) {
-                this.status = 0;
+                if (status === 6) {  // 死亡后就一直处于6状态 ++ 和 -- 抵消
+                    this.frame_current_cnt--;
+                } else {
+                    this.status = 0;
+                }
             }
         }
 
